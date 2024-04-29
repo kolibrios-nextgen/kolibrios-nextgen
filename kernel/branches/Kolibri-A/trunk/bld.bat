de9c66580367aeda9d289f4da94241bd7436084d
Maxim Logaev <maxlogaev@proton.me>
2024-04-29 23:49:02 +0300

    Krn: Drop unnecessary SVN tags and branches
    Signed-off-by: Maxim Logaev <maxlogaev@proton.me>

--- kernel/branches/Kolibri-A/trunk/bld.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-   if not exist bin mkdir bin
-   fasm -m 65536 kernel.asm bin\kernel.mnt
-
-pause
-exit 0
--- kernel/branches/Kolibri-A/trunk/kmake.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This is a simplified script to compile Kolibri-A kernel 
-# please place *fasm program to the topmost KOS directory
-# and create an empty /bin folder there before run this script  
-
-	../../../../../fasm -m 65536 kernel.asm ../../../../../bin/kernel.mnt
-	exit 0
-	
-	
